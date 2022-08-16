b61dd2c39414dc4b03250121d1c3e3409544ac9c
barifahim007 <ehsanulbarifhahim7@gmail.com>
2022-08-17 03:21:05 +0600

    diposit balance

--- javascript/diposit.js
+++ javascript/diposit.js
@@ -8,13 +8,12 @@ document.getElementById('btn-diposit').addEventListener('click', function () {
     const userNewDoller = parseFloat(userDoller);
     const dipositTotal = newDiposit + userNewDoller;
     userDipositDoller.innerText = dipositTotal;
-
-
-
-
-
-
-
+    // current balance addition
+    const currentDipositDoller = document.getElementById('total-balance');
+    const newDipositDoller = currentDipositDoller.innerText;
+    const newTOtalDoller = parseFloat(newDipositDoller);
+    const netDoller = newDiposit + newTOtalDoller;
+    currentDipositDoller.innerText = netDoller;
 
 
 })
